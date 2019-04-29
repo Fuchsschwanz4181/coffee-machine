e8516fc64e3aeb753229074c37ece2ae923ee7e7
Fuchsschwanz4181 <38594013+Fuchsschwanz4181@users.noreply.github.com>
2019-04-29 15:42:18 +0200

    Add files via upload
    hmmm

---
+++ drinks.py
@@ -0,0 +1,5 @@
+ESPRESSO = dict(name="espresso", beans=20, water=30, milk=0)
+LATTE = dict(name="latte", beans=30, water=180, milk=120)
+CAPPUCINO = dict(name="cappucino", beans=15, water=120, milk=180)
+
+drinks = [ESPRESSO, LATTE, CAPPUCINO]
